130ac5667883f9a8d9b84803cc14cb7764f7639d
xuanlinhha <xuanlinhha@gmail.com>
2019-01-01 15:28:25 +0800

    add dynamic test

---
+++ src/wct/resourses/Color.java
@@ -0,0 +1,37 @@
+package wct.resourses;
+
+/**
+ *
+ * @author xuanlinhha
+ */
+public class Color {
+
+    private int red;
+    private int green;
+    private int blue;
+
+    public int getRed() {
+        return red;
+    }
+
+    public void setRed(int red) {
+        this.red = red;
+    }
+
+    public int getGreen() {
+        return green;
+    }
+
+    public void setGreen(int green) {
+        this.green = green;
+    }
+
+    public int getBlue() {
+        return blue;
+    }
+
+    public void setBlue(int blue) {
+        this.blue = blue;
+    }
+
+}
--- src/wct/test/DynCapture.java
+++ src/wct/test/DynCapture.java
@@ -1,21 +1,19 @@
 package wct.test;
 
-import java.awt.AWTException;
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Rectangle;
 import java.awt.Robot;
 import java.awt.Toolkit;
 import java.awt.image.BufferedImage;
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.IOException;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
-import javax.imageio.ImageIO;
-import org.apache.commons.codec.digest.DigestUtils;
+import java.util.Map;
 import wct.resourses.Coordinate;
 import wct.resourses.Keyboard;
-import wct.resourses.Screen;
+import wct.resourses.Mouse;
 
 /**
  *
@@ -23,74 +21,146 @@ import wct.resourses.Screen;
  */
 public class DynCapture {
 
-    public static void main(String[] args) throws AWTException {
+    int red = 197, green = 196, blue = 195;
+    Coordinate corner1 = new Coordinate(73, 990);
+    Coordinate corner2 = new Coordinate(110, 1024);
+    private static final int GRAY_THRESHOLD = 50;
+    private static final double DIFF_THRESHOLD = 0.8;
+
+    public static void main(String[] args) throws Exception {
         DynCapture dc = new DynCapture();
-        dc.capture();
+        dc.sampleRun();
     }
 
-    private void test1() throws AWTException {
-        Robot r = new Robot();
-        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-        Coordinate corner1 = new Coordinate(72, 544);
-        Coordinate corner2 = new Coordinate(112, 582);
-        int fixedX = corner1.getX();
-        int len = corner2.getX() - corner1.getX();
-        //
-        int vertical = fixedX - len / 6;
-        int i = 1;
-        // find Y of gray region
-        int begin = r.getPixelColor(vertical, i).getRGB();
-        while (i < screenSize.height) {
-            int tmp = r.getPixelColor(vertical, i).getRGB();
-            if (tmp == begin) {
-                i++;
-                continue;
-            } else {
-                break;
+    private void sampleRun() throws Exception {
+        Mouse.getInstance().click(new Coordinate(169, 1054));
+        Thread.sleep(1000);
+
+        // load existing data from file
+        boolean isDown = true;
+        List<Map<String, Integer>> database = new ArrayList<Map<String, Integer>>();
+        Map<String, Integer> prev = null;
+        while (true) {
+            BufferedImage avatar = captureAvatar();
+            if (avatar != null) {
+                Map<String, Integer> current = extractData(avatar);
+                if (prev != null && isSame(current, prev)) {
+                    break;
+                } else {
+                    // check if the new avatar has been processed or not
+                    boolean exist = false;
+                    for (Map<String, Integer> m : database) {
+                        if (isSame(current, m)) {
+                            exist = true;
+                            break;
+                        }
+                    }
+                    if (!exist) {
+                        database.add(current);
+                        // processing
+                        Keyboard.getInstance().pasteWithEnter();
+                        // wait for processing
+                        Thread.sleep(1000);
+                    }
+                }
             }
-        }
-        int dynY = i+len/3;
-        
-        // capture image
-        int counter = 0;
-        try {
-            BufferedImage image = r.createScreenCapture(new Rectangle(fixedX, dynY, len, len));
-            ByteArrayOutputStream baos = new ByteArrayOutputStream();
-            ImageIO.write(image, "jpg", baos);
-            //
-            ImageIO.write(image, "jpg", new File("img" + counter + ".jpg"));
-            counter++;
-
-            byte[] bytes = baos.toByteArray();
-            String hash = DigestUtils.sha256Hex(bytes);
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
-    }
-
-    private void capture() {
-        Coordinate c1 = new Coordinate(66, 48);
-        Coordinate c2 = new Coordinate(66, 388);
-        Coordinate c3 = new Coordinate(72, 125);
-        Coordinate c4 = new Coordinate(72, 137);
-        Coordinate c5 = new Coordinate(72, 176);
-        List<Coordinate> dynCoors = new ArrayList<Coordinate>();
-        dynCoors.add(c1);
-        dynCoors.add(c2);
-        dynCoors.add(c3);
-        dynCoors.add(c4);
-        dynCoors.add(c5);
-        try {
-            Screen.getInstance().setDynPositions(dynCoors);
-            Thread.sleep(3000);
-            for (int i = 0; i < 5; i++) {
-                System.out.println(Screen.getInstance().getDynColorData());
+            if (isDown) {
                 Keyboard.getInstance().down(1);
-                Thread.sleep(2000);
+            } else {
+                Keyboard.getInstance().up(1);
             }
-        } catch (Exception e) {
-            e.printStackTrace();
+            Thread.sleep(1000);
+        }
+        
+        // save database
+        System.out.println(database.size());
+    }
+
+    private BufferedImage captureAvatar() throws Exception {
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        Robot r = new Robot();
+        int len = corner2.getX() - corner1.getX();
+        int regionX = corner1.getX() - len / 6;
+        int regionY = len;
+        int regionWidth = 7 * len / 6;
+        int regionHeigth = screenSize.height - len;
+        // capture region
+        BufferedImage regionImg = r.createScreenCapture(new Rectangle(regionX, regionY, regionWidth, regionHeigth));
+        // extract selected image
+        int tmpY = 0;
+        while (tmpY < regionImg.getHeight()) {
+            int clr = regionImg.getRGB(0, tmpY);
+            int tmpRed = (clr & 0x00ff0000) >> 16;
+            int tmpGreen = (clr & 0x0000ff00) >> 8;
+            int tmpBlue = clr & 0x000000ff;
+            int diff = Math.abs(tmpRed - red) + Math.abs(tmpGreen - green) + Math.abs(tmpBlue - blue);
+            if (diff < GRAY_THRESHOLD) {
+                break;
+            } else {
+                tmpY++;
+            }
+        }
+
+        int imgX = len / 6;
+        int imgY = tmpY + len / 3;
+        // if cannot find any selected 
+        if (imgY + len > regionImg.getHeight()) {
+            return null;
+        } else {
+            return regionImg.getSubimage(imgX, imgY, len, len);
         }
     }
 
+    private String dataToString(Map<String, Integer> data) {
+        StringBuilder sb = new StringBuilder();
+        for(String k:data.keySet()){
+            sb.append(k);
+            sb.append(":");
+            sb.append(data.get(k));
+            sb.append(" ");
+        }
+        return sb.toString();
+    }
+    
+    private Map<String, Integer> extractData(BufferedImage img) {
+        Map<String, Integer> data = new HashMap<String, Integer>();
+        for (int i = 0; i < img.getWidth(); i++) {
+            for (int j = 0; j < img.getHeight(); j++) {
+                int clr = img.getRGB(i, j);
+                int red = (clr & 0x00ff0000) >> 16;
+                int green = (clr & 0x0000ff00) >> 8;
+                int blue = clr & 0x000000ff;
+                String key = Integer.toString(red) + Integer.toString(green) + Integer.toString(blue);
+                data.put(key, data.containsKey(key) ? (data.get(key) + 1) : 1);
+            }
+        }
+        return data;
+    }
+
+    private boolean isSame(Map<String, Integer> data1, Map<String, Integer> data2) {
+        double count = 0;
+        for (String s : data2.keySet()) {
+            if (data1.containsKey(s)) {
+                count += (int) Math.min(data1.get(s), data2.get(s));
+            }
+        }
+        double total = 0;
+        for (int i : data1.values()) {
+            total += i;
+        }
+        DecimalFormat df = new DecimalFormat("#.##");
+        return (Double.valueOf(df.format(count / total)) >= DIFF_THRESHOLD);
+    }
+
+    private void printColor() throws Exception {
+        Mouse.getInstance().click(new Coordinate(169, 1054));
+        Thread.sleep(1000);
+
+        Robot r = new Robot();
+        while (true) {
+            Color tmp = r.getPixelColor(0, 0);
+            System.out.println("Color = " + tmp);
+
+        }
+    }
 }
